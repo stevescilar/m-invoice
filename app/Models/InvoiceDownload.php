0c18210b89a9ee1b64e9c54dcffdfb2d7e65ecd8
stevescilar <stevemuambi@gmail.com>
2026-02-28 01:28:59 +0300

    refactor: Update InvoiceDownload model structure and remove unnecessary code

--- app/Models/InvoiceDownload.php
+++ app/Models/InvoiceDownload.php
@@ -1,15 +1,4 @@
 <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class InvoiceDownload extends Model
-{
-    //
-}
-<?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
